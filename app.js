1d137c9ba660d528016d8462e5c9a0276d5d6d9f
azimjon-95 <azimjon995@gmail.com>
2022-07-15 13:32:35 +0500

    Apple Watch

---
+++ app.js
@@ -0,0 +1,11 @@
+const topModal = document.querySelector(".top_modal")
+
+function showTopModal(){
+    topModal.style.top = "43px"
+    topModal.style.opacity = 1
+}
+
+function hideTopModal(){
+    topModal.style.top = "-400px"
+    topModal.style.opacity = 0
+}
